6ad241ca54aa8d19e5873c6e18229604b992cd6a
Drmrsan <drmrsan@gmail.com>
2017-02-09 22:21:25 +0100

    rack-cors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |d|
+FreelanceDocument.create!(
+	title:"Document #{d}", 
+	description:"Yuccie put a bird on it everyday carry, pug echo park edison bulb meh paleo. Chillwave wayfarers stumptown, gochujang cardigan authentic shabby chic. 8-bit taxidermy bitters four loko. Selfies kitsch offal cliche, live-edge pug prism. Lomo stumptown pickled flexitarian. Dreamcatcher pabst activated charcoal iceland echo park enamel pin, neutra messenger bag. Cold-pressed kitsch selvage green juice flexitarian, williamsburg skateboard tousled freegan.",
+	file_url:"https://docs.google.com/document/d/1iDrDiDN2DcZoteA_2EQzKoCRTzgotXiIfdoQiA1dqYY/edit?usp=sharing",
+	image_url:"https://static.pexels.com/photos/296878/pexels-photo-296878.jpeg")
+end
